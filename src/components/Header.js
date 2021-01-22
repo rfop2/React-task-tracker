155ca5596359dfd3d627af1c4b284175d75a8a9b
Rodrigo <rfop@cin.ufpe.br>
2021-01-21 22:08:49 -0300

    coloquei onToggler e onDelete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 
 import PropTpyes from 'prop-types';
+import Button from './Button'
 
 export const Header = ({title}) => {
+    const onClick = () => {
+        console.log("Click");
+    }
     return (
         <header className="header">
             <h1>{title}</h1>
-            <button className="btn">
-                Add
-            </button>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
